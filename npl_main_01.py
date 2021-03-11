00d57ab7fd2002218cc8c488f1344d86331723fc
Fangan998 <50802235+Fangan998@users.noreply.github.com>
2021-03-11 11:58:10 +0800

    Add files via upload

--- npl_main_01.py
+++ npl_main_01.py
@@ -1,7 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 11 10:37:53 2021
+dict_word = {}
+f = open('dec/單字詞_13053(注音_無聲調).dic','r',encoding='utf-8')
 
-@author: CJCU-CC
-"""
+for i in f.readlines():
+    temp = i.strip().split(',')
+    if len(temp)>=2:
+        dict_word[temp[0]]= temp[1]   
+f.close()
 
+num_of_phonemes=0
+dict_phoneme={}
+
+f01=open('test_data/Gossiping-QA-Dataset.txt','r',encoding='utf-8')
+for line in f01:
+    for w in line:
+        if w in dict_word:
+            syllable = dict_word[w]
+            for subsyllable in syllable:
+                if subsyllable in dict_phoneme:
+                    dict_phoneme[subsyllable] = dict_phoneme[subsyllable] + 1
+                else:
+                    dict_phoneme[subsyllable] = 1
+f01.close()
+
+print(sorted(dict_phoneme.items(),key=lambda x:x[1]))
